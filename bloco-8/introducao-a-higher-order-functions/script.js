8d26ea2e7926795f5c3e6defa29d9cd9f2e80b15
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-03-10 08:12:54 -0300

    conclui exercicios dia 8.1

--- bloco-8/introducao-a-higher-order-functions/script.js
+++ bloco-8/introducao-a-higher-order-functions/script.js
@@ -60,29 +60,3 @@ const result = (resposta, resposta2, callback) => {
 }
 
 console.log(result(RIGHT_ANSWERS, STUDENT_ANSWERS, verify));
-
-const mage = {
-    healthPoints: 130,
-    intelligence: 45,
-    mana: 125,
-    damage: undefined,
-  };
-  
-  const warrior = {
-    healthPoints: 200,
-    strength: 30,
-    weaponDmg: 2,
-    damage: undefined,
-  };
-  
-  const dragon = {
-    healthPoints: 350,
-    strength: 50,
-    damage: undefined,
-  };
-  
-  const battleMembers = { mage, warrior, dragon };
-
-  const dano = () => {
-    
-  }
